7f9c609fd405c45215b3561f34269b4cfa537d84
melcar <15875473+melcar@users.noreply.github.com>
2024-02-18 14:18:14 +0100

    11,122 + quizz2

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -14,14 +14,33 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
+}
+fn insert_score(scores: &mut HashMap<String, Team>, name: String, scored: u8, conceded: u8) -> () {
+    scores.insert(
+        name,
+        Team {
+            goals_scored: scored,
+            goals_conceded: conceded,
+        },
+    );
+}
+
+fn update_score(scores: &mut HashMap<String, Team>, name: String, scored: u8, conceded: u8) -> () {
+    match scores.get(&name) {
+        None => insert_score(scores, name, scored, conceded),
+        Some(n) => insert_score(
+            scores,
+            name,
+            n.goals_scored + scored,
+            n.goals_conceded + conceded,
+        ),
+    }
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -34,11 +53,8 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded by team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+        update_score(&mut scores, team_1_name, team_1_score, team_2_score);
+        update_score(&mut scores, team_2_name, team_2_score, team_1_score);
     }
     scores
 }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -30,13 +28,20 @@ pub enum Command {
 
 mod my_module {
     use super::Command;
-
+    use super::Command::{Append, Trim, Uppercase};
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            match command {
+                Uppercase => output.push(string.to_uppercase()),
+                Trim => output.push(string.trim().to_string()),
+                Append(n) => output.push(
+                    string.to_owned()
+                        + &vec!["bar".to_string(); *n].into_iter().collect::<String>(),
+                ),
+            }
         }
         output
     }
@@ -45,7 +50,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
